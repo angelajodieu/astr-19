34c7c6dc65ac5388b605097edafed08eeb358cef
Angela U <148512852+angelajodieu@users.noreply.github.com>
2025-10-01 23:10:17 -0700

    Session 1 Prompt for Coding Journal 1

---
+++ print-name.py
@@ -0,0 +1,9 @@
+#code for journal one, session one
+#print out name and facts about me
+
+def main():
+    print("My name is Angela Jodie Untalan (she/her)")
+    print("My favorite movie is The Lego Batman movie! My favorite food is hot pot because I like how you could eat pretty much anything with it, like rice, meat, and vegetables.")
+
+if __name__=="__main__":
+	main()
